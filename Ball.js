756ef50739750eb9727371f180e1349f91ea5471
JM <jacobangga@mcgi.org>
2023-03-09 21:30:20 -0500

    Mechanics implemented There are a few bugs

--- Ball.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const Ball = {
-    x: canvas.width/2,
-    y: 100,
-    size: 7.5,
-    dx
-}
--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let p1 = 0;
+let p2 = 0;
+
 const Player = {
 	w: 50,
 	h: 70,
 	x: 150,
 	y: canvas.height/2-45,
 	speed: 5,
-	dx: 0,
 	dy: 0
+}
+
+const AI = {
+	w: 50,
+	h: 70,
+	x: 850,
+	y: canvas.height/2-45,
+	dy: 4
 }
 
 const ball = {
@@ -19,20 +29,48 @@ const ball = {
 	dy: 5
 };
 
+function resetball() {
+	ball.x = canvas.width/2;
+	ball.y = 100;
+}
+
+function resetPlayer() {
+	Player.x = 150
+	Player.y = canvas.height/2-45
+	Player.dy = 0
+}
+
 function drawPlayer() {
-	ctx.beginPath();//
+	ctx.beginPath();
 	ctx.moveTo(Player.x,
-		Player.y);//
+		Player.y);
 	ctx.lineTo(Player.x,
-		Player.y + Player.h);//
+		Player.y + Player.h);
 	ctx.arc(
 		Player.x-10,
 		Player.y + Player.h,
 		10,
 		0,
-		Math.PI);//
+		Math.PI);
 	ctx.lineTo(Player.x-20, Player.y);
 	ctx.arc(Player.x-10, Player.y, 10, 0, Math.PI, true);
+	ctx.fill();
+}
+
+function drawAI() {
+	ctx.beginPath();
+	ctx.moveTo(AI.x,
+		AI.y);
+	ctx.lineTo(AI.x,
+		AI.y + AI.h);
+	ctx.arc(
+		AI.x-10,
+		AI.y + AI.h,
+		10,
+		0,
+		Math.PI);
+	ctx.lineTo(AI.x-20, AI.y);
+	ctx.arc(AI.x-10, AI.y, 10, 0, Math.PI, true);
 	ctx.fill();
 }
 
@@ -51,6 +89,15 @@ function detectWalls() {
 	}
 }
 
+function detectWallsAI() {
+	if (AI.y-10 < 0){
+		AI.y = 10;
+	}
+	if (AI.y+80 > canvas.height){
+		AI.y = canvas.height-80;
+	}
+}
+
 function move() {
 	Player.y += Player.dy;
 
@@ -64,13 +111,40 @@ function moveDown() {
 	Player.dy = Player.speed;
 }
 
+function chaseball() {
+	if (AI.y+35 > ball.y){
+		AI.y -= AI.dy;
+	}
+	else if (AI.y+35 < ball.y) {
+		AI.y += AI.dy;
+	}
+}
+
+function paddlehit() {
+	return ((ball.x - ball.size <= Player.x && ball.x + ball.size >= Player.x-3) && (ball.y - ball.size >= Player.y-10 && ball.y + ball.size <= Player.y+Player.h+10))
+}
+
+function paddlehitAI() {
+	return ((ball.x + ball.size >= AI.x-20 && ball.x - ball.size <= AI.x-17) && (ball.y - ball.size >= AI.y-10 && ball.y + ball.size <= AI.y+AI.h+10))
+}
+
+function edgehit() {
+	//When it hits the side of the paddle
+	return (((ball.y+ball.size >= Player.y-10 && ball.y+ball.size <= Player.y-5) //Top side
+			|| (ball.y-ball.size <= Player.y+Player.h+10 && ball.y-ball.size >= Player.y+Player.h+5)) //Bottom side
+			&& (ball.x-ball.size <= Player.x && ball.x+ball.size >= Player.x-20))//x range
+}
+
+function edgehitAI() {
+	return (((ball.y+ball.size <= AI.y-10 && ball.y+ball.size >= AI.y-7)//Top side
+			|| (ball.y-ball.size >= Player.y+Player.h+10 && ball.y-ball.size <= Player.y+Player.h+7))//Bottom side
+			&& (ball.x-ball.size >= AI.x && ball.x+ball.size <= Player.x-20))//x range
+}
 
 function keyDown(e) {
 	if (e.key === 'ArrowUp' || e.key === 'Up'){
-		console.log("we up")
 		moveUp();
 	} else if (e.key === 'ArrowDown' || e.key === 'Down') {
-		console.log("we down")
 		moveDown();
 	}
 }
@@ -87,20 +161,27 @@ function update() {
 	drawPlayer();
 	move();
 	drawball();
-	
-	ball.x += ball.dx;
-	ball.y += ball.dy;
+	drawAI();
+	chaseball();
+	detectWallsAI();
 
-	if (ball.x + ball.size >= canvas.width || ball.x - ball.size <= 0) {
+	if (paddlehit() || paddlehitAI() || edgehit() || edgehitAI()) {
 		ball.dx *= -1;
 	}
 	if (ball.y + ball.size >= canvas.height || ball.y - ball.size <= 0) {
 		ball.dy *= -1;
 	}
+	
+	ball.x += ball.dx;
+	ball.y += ball.dy;
+
+	if (ball.x + ball.size <= 0 || ball.x - ball.size >= canvas.width) {
+		resetball();
+	}
 
 	requestAnimationFrame(update);
-
 }
+
 update();
 
 document.addEventListener('keydown', keyDown);
